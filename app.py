87fc0fa1e5fd60fe8dbddb4542a1885371755ada
Everardo Maya <maya.everardo@gmail.com>
2021-07-15 20:59:33 -0700

    initial rev for flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, jsonify, make_response, Response
+import numpy as np
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
