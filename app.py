01d98212a09e2ba5f24d39062459b7b6f924d57c
Everardo Maya <maya.everardo@gmail.com>
2021-07-22 05:13:07 +0000

    updated json response with prediction

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ import cv2 as cv
 import numpy as np
 import os
 import tensorflow as tf
+from tensorflow.math import argmax 
 
 app = Flask(__name__)
 
 trained_models = {
-    'model1': {'': os.path.join('.', 'Resources', 'cnn_model.h5'), 'dim': (120, 187)},
-    'model2': {'path': os.path.join('.', 'Resources', 'em_model-rgb-adam-weighted.h5'), 'dim': (120, 187)}
+    'model1': {'path': os.path.join('.', 'Resources', 'cnn_model.h5'), 'dim': (120, 187)},
+    'model2': {'path': os.path.join('.', 'Resources', 'em_model-rgb-adam-weighted.h5'), 'dim': (120, 187), 'decode': {0: 'benign', 1: 'malignant'}},
+    'model3': {'path': os.path.join('.', 'Resources', 'em_model-rgb.h5'), 'dim': (120, 187), 'decode': {0: 'benign', 1: 'malignant'}}
 }
 
 @app.route("/process-image", methods=['POST'])
@@ -26,15 +28,22 @@ def predict_image():
         im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
         img = cv.imdecode(im_arr, flags=cv.IMREAD_COLOR)
         
-        #todo
         model_details = trained_models['model2'];
         model = load_model(model_details['path'])
         resized_img = cv.resize(img, model_details['dim'])
-        prediction = model.predict(resized_img.reshape((1, model_details['dim'][0], model_details['dim'][1])))
+        prediction = model.predict(resized_img.reshape((1, model_details['dim'][0], model_details['dim'][1], 3)))
 
-        pprint(prediction) 
+        # decode prediction
+        argmax_val = argmax(prediction, axis=1)  
+        text_prediction = model_details['decode'][int(argmax_val[0])]
 
-        return make_response(jsonify({'file_token': file_token, 'img_shape': img.shape}))
+        return make_response(jsonify({
+                'file_token': file_token,
+                'img_shape': img.shape,
+                'resize_shape': resized_img.shape,
+                'prediction': str(list(prediction[0])),
+                'text_prediction': text_prediction
+            }))
     else:
         return make_response(jsonify({'wtf': 'wtf'}))
 
