5e533eea0c786a19d7c89ef7df3bb6f8138586e8
Everardo Maya <maya.everardo@gmail.com>
2021-07-22 04:15:47 +0000

    added logic to load models

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ import base64
 import cv2 as cv
 import numpy as np
 import os
+import tensorflow as tf
 
 app = Flask(__name__)
+
+trained_models = {
+    'model1': {'': os.path.join('.', 'Resources', 'cnn_model.h5'), 'dim': (120, 187)},
+    'model2': {'path': os.path.join('.', 'Resources', 'em_model-rgb-adam-weighted.h5'), 'dim': (120, 187)}
+}
 
 @app.route("/process-image", methods=['POST'])
 def predict_image():
@@ -21,10 +27,13 @@ def predict_image():
         img = cv.imdecode(im_arr, flags=cv.IMREAD_COLOR)
         
         #todo
-        load_model(None)
-        resize_image(img)
-        predict_image(img)
-        
+        model_details = trained_models['model2'];
+        model = load_model(model_details['path'])
+        resized_img = cv.resize(img, model_details['dim'])
+        prediction = model.predict(resized_img.reshape((1, model_details['dim'][0], model_details['dim'][1])))
+
+        pprint(prediction) 
+
         return make_response(jsonify({'file_token': file_token, 'img_shape': img.shape}))
     else:
         return make_response(jsonify({'wtf': 'wtf'}))
@@ -33,7 +42,8 @@ def resize_image(img):
     pass
 
 def load_model(model):
-    pass
+    pprint(model)
+    return tf.keras.models.load_model(model)
 
 def predict_image(img):
     pass
